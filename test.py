7ff7f2a3b6b90e3fee02c94593cf9d4bbafd2393
shengpeng319 <shengpeng319@gmail.com>
2021-06-10 00:01:59 -0500

    asdf

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
-x = "abc"
-del x
-print (dir())
-print(x)
+
+myinput = input("Please input the numbers split with comma: ")
+
+mylist = myinput.split(',')
+
+result = 0
+
+for i in mylist:
+    result = result + int(i)
+
+print("The calculation result is ", result)
